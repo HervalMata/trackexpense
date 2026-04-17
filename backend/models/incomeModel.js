dbf0b5d111e837c8163ba9001707dd0571539702
Herval Mata <hervalmataamparo@gmail.com>
2026-04-17 16:16:39 -0300

    :sparkles: feat: track expense - create income model

---
+++ backend/models/incomeModel.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const incomeSchema = new mongoose.Schema({
+    description: {
+        type: String,
+        required: true,
+    },
+    amount: {
+        type: Number,
+        required: true,
+    },
+    category: {
+        type: String,
+        required: true,
+    },
+    date: {
+        type: Date,
+        required: true,
+    },
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+    type: {
+        type: String,
+        default: 'income',
+    }
+}, { timestamps: true });
+
+const IncomeModel = mongoose.models.Income || mongoose.model('Income', incomeSchema)
+export default IncomeModel
